1787836eb2d299abe1407e9c76b5342cbb23ac87
R.Lee <1816212157@qq.com>
2016-09-16 22:46:02 +0800

    Update Test.java

--- Leetcode6/src/com/solution263/Test.java
+++ Leetcode6/src/com/solution263/Test.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class Test {
 
-	public boolean isUgly(int num) {//分支判断法
+	public boolean isUgly(int num) {//路脰脰搂脜脨露脧路篓
 		if(num<=0){
 			return false;
 		}
 		while(num>1){
 			if(num%2==0){
 				num=num/2;
+				return isUgly(num);
 			}
-			if(num%3==0){
+			else if(num%3==0){
 				num=num/3;
+				return isUgly(num);
 			}
-			if(num%5==0){
+			else if(num%5==0){
 				num=num/5;
+				return isUgly(num);
 			}
-			if(num%2!=0 && num%3!=0 && num%5!=0 && num>1){
+			else{
 				return false;
 			}
 		}
